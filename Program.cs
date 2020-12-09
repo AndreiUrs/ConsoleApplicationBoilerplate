4b6129b040e6cd26223998bc452d95199b71c6f4
AndreiUrs <ursachi.andrei1991@gmail.com>
2020-12-09 19:31:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplicationBoilerplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
